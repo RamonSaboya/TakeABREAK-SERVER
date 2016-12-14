08b430363bec0fdfce726e085ba5a5ca7420a7ba
dsl <dsl@G1C31.windows.cin.ufpe.br>
2016-12-14 10:47:00 -0300

    Server GUI

--- src/br/ufpe/cin/if678/gui/TakeABREAKServer.java
+++ src/br/ufpe/cin/if678/gui/TakeABREAKServer.java
@@ -4,16 +4,34 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Set;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
 
 import br.ufpe.cin.if678.ServerController;
+import br.ufpe.cin.if678.util.Pair;
+import br.ufpe.cin.if678.util.Tuple;
 
 @SuppressWarnings("serial")
-public class TakeABREAKServer extends JFrame {
+public class TakeABREAKServer extends JFrame implements Runnable {
 
 	public static final Color BACKGROUND_COLOR = new Color(220, 220, 220);
 
+	private JTable table;
+	private JScrollPane scrollPane;
+	
+	private String[] titles = {"Nome", "IP", "Status"};
+	private String[][] data;
+	private ServerController controller;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -43,7 +61,17 @@ public class TakeABREAKServer extends JFrame {
 		setBackground(BACKGROUND_COLOR);
 		getContentPane().setLayout(null);
 
-		ServerController controller = ServerController.getInstance();
+		table = new JTable(data, titles);
+		
+		scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(10, 11, 1174, 650);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+	
+		getContentPane().add(scrollPane);
+		
+		controller = ServerController.getInstance();
 
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -51,5 +79,29 @@ public class TakeABREAKServer extends JFrame {
 				controller.exit();
 			}
 		});
+	}
+	
+	public void run(){
+		getInfo();
+		
+		DefaultTableModel dtm = new DefaultTableModel();
+		dtm.addRow(data);
+		table.setModel(dtm);
+	}
+	
+	public void getInfo(){
+		HashMap<String, Integer> nameToID = controller.getNameToID();
+		Set<String> nameSet = nameToID.keySet();
+		String[] name = new String[nameSet.size()];
+		nameSet.toArray(name);
+		HashMap<Integer, Pair<String, InetSocketAddress>> IDtoNameAdress = controller.getIDToNameAddress();
+
+		data = new String[3][name.length];
+		for(int i = 0; i < name.length; i++){
+			data[i][0] = name[i];
+			data[i][1] = IDtoNameAdress.get(nameToID.get(name[i])).getFirst();
+			data[i][2] = controller.getQueuedMessages(nameToID.get(name[i])).isEmpty() ? "0" : "1" ;
+		}
+		
 	}
 }
