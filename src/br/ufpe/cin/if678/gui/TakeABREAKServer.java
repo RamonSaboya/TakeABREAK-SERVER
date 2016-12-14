7a654b83931314b5874867cbe389256ebd13ee31
dsl <dsl@G1C31.windows.cin.ufpe.br>
2016-12-14 12:24:22 -0300

    Server

--- src/br/ufpe/cin/if678/gui/TakeABREAKServer.java
+++ src/br/ufpe/cin/if678/gui/TakeABREAKServer.java
@@ -4,30 +4,25 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.table.DefaultTableModel;
 
 import br.ufpe.cin.if678.ServerController;
-import br.ufpe.cin.if678.util.Pair;
 
 @SuppressWarnings("serial")
 public class TakeABREAKServer extends JFrame {
 
 	public static final Color BACKGROUND_COLOR = new Color(220, 220, 220);
 
-	private JTable table;
+	private static JTable table;
 	private JScrollPane scrollPane;
 
 	private String[] titles = { "Nome", "IP", "Status" };
 	private String[][] data;
-	private ServerController controller;
+	private static ServerController controller;
 
 	/**
 	 * Launch the application.
@@ -40,6 +35,7 @@ public class TakeABREAKServer extends JFrame {
 					TakeABREAKServer frame = new TakeABREAKServer();
 					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -77,27 +73,4 @@ public class TakeABREAKServer extends JFrame {
 		});
 	}
 
-	public void run() {
-		getInfo();
-
-		DefaultTableModel dtm = new DefaultTableModel();
-		dtm.addRow(data);
-		table.setModel(dtm);
-	}
-
-	public void getInfo() {
-		HashMap<String, Integer> nameToID = controller.getNameToID();
-		Set<String> nameSet = nameToID.keySet();
-		String[] name = new String[nameSet.size()];
-		nameSet.toArray(name);
-		HashMap<Integer, Pair<String, InetSocketAddress>> IDtoNameAdress = controller.getIDToNameAddress();
-
-		data = new String[3][name.length];
-		for (int i = 0; i < name.length; i++) {
-			data[i][0] = name[i];
-			data[i][1] = IDtoNameAdress.get(nameToID.get(name[i])).getFirst();
-			data[i][2] = controller.getQueuedMessages(nameToID.get(name[i])).isEmpty() ? "0" : "1";
-		}
-
-	}
 }
